a1b625182cd320af8a14a275b48a885ee3cb3b55
harrisonywu <harrisonywu@gmail.com>
2019-03-18 12:06:39 -0700

    Implement board in index.html and pseudocode ideas for app functionality

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,28 @@
+// what do i need to do
+/* 
+1. instantiate what the board should first look like when loaded
+2. on click...
+    a. place a marker at that position (rerender the button so it includes an x/o in it)
+        The click is player X
+    b. check if this is a winning play (or a tie)
+        IF it is, then send alert -> winning message
+    c. prevent being clicked on again
+    d. switch the next click to be that of player O
+    e. 
+3. have a button available that always resets the game to starting board state
+    remember, player X must be first again
+4. 
 
+we are going to add EventListeners to... something.
+
+
+
+EXTRA
+add a button to the winning/tie alert that will allow for game to be reset
+
+*/
+
+var buttons = document.getElementsByClassName('button');
+
+// var mainTarget = ________.addEventListener();
 
