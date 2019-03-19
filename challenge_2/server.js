a50139c83f77a240d6ae3a0de9cac6de4be77761
Harrison Wu <harrisonywu@gmail.com>
2019-03-19 12:05:39 -0700

    Set up server using express and make basic index.html with form submit tags

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 
+// this will receive the post request and do something with it
+// app.post()
+
+
+
+app.listen(port, () => console.log('hello world from app.listen'));
 
