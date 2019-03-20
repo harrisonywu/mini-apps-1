00656c80ed2a423206c42cf608f8db011d28eb82
Harrison Wu <harrisonywu@gmail.com>
2019-03-19 20:21:23 -0700

    Implement partial JSON -> CSV parser, currently grabs the data as an object

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -5,24 +5,20 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
-
-// what does the below line do? I'm not sure
-// if this is set up correctly, it does exactly what live-server does
 app.use(express.static('./client'));
 app.use(bodyParser.urlencoded({extended: false}));
 
-
-app.post('/', function(req,res) {
+app.post('/', function(req, res) {
   // turn JSON into CSV
-  console.log('my request body data: ', req.body.inputText)
-  // request body is undefined
-  // DON'T RES.REDIRECT
-  // definitely use preventDefault
+  var formInput = req.body.inputText;
+  var reportData = formInput.slice(0);
+  var parsedData = JSON.parse(reportData)
+  console.log('my request body data: ', typeof(parsedData));
+  // parsedData is now an object
+
   // res.end probably
   res.end();
 })
 
-
-
-app.listen(port, () => console.log('hello world from app.listen'));
+app.listen(port, () => console.log('express server listening on ', port));
 
